1674f5c07a25226f5b2f1b2a74ca667ed34d9ce2
abdelilah2005 <abdelilahessemlali@gmail.com>
2024-11-03 18:46:57 +0100

    J'ai termine la fonction de supperimer

--- app.js
+++ app.js
@@ -6,10 +6,20 @@ let affiche = document.querySelector('.p')
 let span = document.querySelector('.span')
 let suppr = document.querySelector('.suppr')
 let tableau = []
-/*---------------------definition du variables--------------------- */
+/*---------------------definition du fonction d'ajout--------------------- */
 ajouter.onclick= function(e){
     tableau.push(input.value)
-    
-    affiche.innerHTML=tableau
-    
+     affiche.innerHTML=tableau
+    input.value = ""
     }
+    /*---------------------definition du fonction de suprumer--------------------- */
+    delet.onclick= function(ev){
+         if (tableau.includes(input.value)) {
+          let recherch = tableau.indexOf(input.value)
+          tableau.splice(recherch,1)
+          affiche.innerHTML=tableau
+          input.value = ""
+        }else{
+           span.innerHTML='makaynax'
+        }
+     }
