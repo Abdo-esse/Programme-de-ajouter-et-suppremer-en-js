0a8b24d67f8f64ac4b64c075fe90b11e51da1392
abdelilah2005 <abdelilahessemlali@gmail.com>
2024-11-03 18:32:46 +0100

    J'ai termine la fonction d'ajoute*

--- app.js
+++ app.js
@@ -6,3 +6,10 @@ let affiche = document.querySelector('.p')
 let span = document.querySelector('.span')
 let suppr = document.querySelector('.suppr')
 let tableau = []
+/*---------------------definition du variables--------------------- */
+ajouter.onclick= function(e){
+    tableau.push(input.value)
+    
+    affiche.innerHTML=tableau
+    
+    }
